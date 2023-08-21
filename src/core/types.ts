7d61b6ed30becb3b905a4c1c421902c2973e1642
Colin Duffy <colin@tomorrowevening.com>
2023-08-20 17:35:32 -0700

    fix(code): minor cleanup

--- src/core/types.ts
+++ src/core/types.ts
@@ -1,6 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
 export type ApplicationMode = 'listener' | 'editor'
 
 export type noop = () => void
