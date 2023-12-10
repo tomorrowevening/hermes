8a7f86c7c05a1bb7b363a57ad5767d7c15741915
Colin Duffy <colin@tomorrowevening.com>
2023-12-10 13:26:11 -0800

    feat(utils): OBJ to HEX

--- src/editor/utils.ts
+++ src/editor/utils.ts
@@ -18,3 +18,20 @@ export function isColor(obj: any) {
     obj.b !== undefined
   );
 }
+
+export function colorToHex(obj: any) {
+  const r = Math.round(obj.r * 255);
+  const g = Math.round(obj.g * 255);
+  const b = Math.round(obj.b * 255);
+
+  const toHex = (value: number) => {
+    const hex = value.toString(16);
+    return hex.length === 1 ? '0' + hex : hex;
+  };
+
+  const red = toHex(r);
+  const green = toHex(g);
+  const blue = toHex(b);
+
+  return '#' + red + green + blue;
+}
