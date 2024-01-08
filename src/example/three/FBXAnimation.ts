7e582c490b33f529552f2c01fef11dc1373faf02
Colin Duffy <colin@tomorrowevening.com>
2024-01-08 09:53:53 -0800

    fix(model): path + shadows enabled

--- src/example/three/FBXAnimation.ts
+++ src/example/three/FBXAnimation.ts
@@ -9,9 +9,19 @@ export default class FBXAnimation extends Object3D {
     this.name = source.replaceAll(' ', '').split('.')[0];
     this.scale.setScalar(0.5);
 
-    new FBXLoader().loadAsync(`models/${source}`)
+    new FBXLoader()
+      .setPath('./models/')
+      .loadAsync(source)
       .then((model: Group) => {
         this.add(model);
+
+        // Shadows
+        model.traverse((obj: Object3D) => {
+          if (obj['isMesh']) {
+            obj.castShadow = true;
+            obj.receiveShadow = true;
+          }
+        });
 
         this.mixer = new AnimationMixer(model);
         const action = this.mixer.clipAction(model.animations[0]);
