78157f57ebf5f65e7d324fdab1e8c96dc7500c9a
Colin Duffy <colin@tomorrowevening.com>
2024-01-10 21:04:00 -0800

    fix(multiview): structure re-ordered

--- src/index.ts
+++ src/index.ts
@@ -15,13 +15,13 @@ export { default as DropdownItem } from './editor/components/DropdownItem';
 export { default as Dropdown } from './editor/components/Dropdown';
 export { default as RemoteController } from './core/RemoteController';
 // RemoteThree
-export { default as InfiniteGridHelper } from './editor/sceneHierarchy/inspector/MultiView/InfiniteGridHelper';
-export { default as UVMaterial } from './editor/sceneHierarchy/inspector/MultiView/UVMaterial';
+export { default as InfiniteGridHelper } from './editor/multiView/InfiniteGridHelper';
+export { default as UVMaterial } from './editor/multiView/UVMaterial';
 export { default as SceneHierarchy } from './editor/sceneHierarchy/SceneHierarchy';
 export { default as Accordion } from './editor/sceneHierarchy/Accordion';
 export { default as ChildObject } from './editor/sceneHierarchy/ChildObject';
 export { default as ContainerObject } from './editor/sceneHierarchy/ContainerObject';
 export { default as Inspector } from './editor/sceneHierarchy/inspector/Inspector';
 export { default as SceneInspector } from './editor/sceneHierarchy/inspector/SceneInspector';
-export { default as MultiView } from './editor/sceneHierarchy/inspector/MultiView/MultiView';
+export { default as MultiView } from './editor/multiView/MultiView';
 export { default as Editor } from './editor/Editor';
