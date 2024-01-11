a7d4a6937282c0cc57701fbe8de53faae8ab3c88
Colin Duffy <colin@tomorrowevening.com>
2024-01-10 18:22:31 -0800

    feat(uvMaterial): added for debugging

---
+++ src/editor/sceneHierarchy/inspector/MultiView/UVMaterial.ts
@@ -0,0 +1,55 @@
+import { ShaderMaterial } from "three";
+
+const vertex = `#include <common>
+#include <batching_pars_vertex>
+#include <uv_pars_vertex>
+#include <color_pars_vertex>
+#include <morphtarget_pars_vertex>
+#include <skinning_pars_vertex>
+#include <logdepthbuf_pars_vertex>
+#include <clipping_planes_pars_vertex>
+
+void main() {
+	#include <uv_vertex>
+	#include <color_vertex>
+	#include <morphcolor_vertex>
+	#include <batching_vertex>
+
+	#if defined ( USE_SKINNING )
+		#include <beginnormal_vertex>
+		#include <morphnormal_vertex>
+		#include <skinbase_vertex>
+		#include <skinnormal_vertex>
+		#include <defaultnormal_vertex>
+	#endif
+
+	#include <begin_vertex>
+	#include <morphtarget_vertex>
+	#include <skinning_vertex>
+	#include <project_vertex>
+	#include <logdepthbuf_vertex>
+	#include <clipping_planes_vertex>
+	#include <worldpos_vertex>
+}`;
+
+const fragment = `
+#include <common>
+#include <uv_pars_fragment>
+#include <clipping_planes_pars_fragment>
+
+void main() {
+	#include <clipping_planes_fragment>
+	gl_FragColor = vec4(vec3(vUv, 0.0), 1.0);
+}`;
+
+export default class UVMaterial extends ShaderMaterial {
+  constructor() {
+    super({
+      defines: {
+        USE_UV: ''
+      },
+      vertexShader: vertex,
+      fragmentShader: fragment,
+    });
+  }
+}
