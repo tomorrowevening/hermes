142d0681d8f75c677d49fe2b23f63bbf23c052cd
Colin Duffy <colin@tomorrowevening.com>
2024-02-23 13:59:25 -0800

    fix(sidePanel): component definition

--- src/editor/sidePanel/SidePanel.tsx
+++ src/editor/sidePanel/SidePanel.tsx
@@ -1,5 +1,5 @@
 // Libs
-import { Component, ReactNode } from 'react';
+import { useEffect, useState } from 'react';
 // Models
 import { debugDispatcher, ToolEvents } from '../global';
 // Components
@@ -8,57 +8,30 @@ import Accordion from './Accordion';
 import ContainerObject from './ContainerObject';
 import Inspector from './inspector/Inspector';
 import { SidePanelState } from './types';
-import RemoteThree from '@/core/remote/RemoteThree';
 
-export default class SidePanel extends Component<SidePanelState> {
-  private three: RemoteThree;
+export default function SidePanel(props: SidePanelState) {
+  const [scene, setScene] = useState(props.scene);
 
-  constructor(props: SidePanelState) {
-    super(props);
-    this.state = {
-      scene: props.scene !== undefined ? props.scene : null,
+  useEffect(() => {
+    const onSetScene = (evt: any) => {
+      setScene(evt.value);
     };
-    this.three = props.three;
-    debugDispatcher.addEventListener(ToolEvents.SET_SCENE, this.setScene);
-  }
+    debugDispatcher.addEventListener(ToolEvents.SET_SCENE, onSetScene);
+    return () => {
+      debugDispatcher.removeEventListener(ToolEvents.SET_SCENE, onSetScene);
+    };
+  }, []);
 
-  componentWillUnmount(): void {
-    debugDispatcher.removeEventListener(ToolEvents.SET_SCENE, this.setScene);
-  }
-
-  render(): ReactNode {
-    const hasScene = this.componentState.scene !== null;
-    const HierarchyName = 'Hierarchy - ' + (hasScene ? `${this.componentState.scene?.name}` : 'No Scene');
-    return (
-      <div id='SidePanel' key='SidePanel'>
-        {(
-          <>
-            <Accordion label={HierarchyName} open={true}>
-              <>
-                {hasScene && (
-                  <ContainerObject child={this.componentState.scene!} three={this.three} />
-                )}
-              </>
-            </Accordion>
-
-            <Inspector three={this.three} />
-          </>
-        )}
-    </div>
-    );
-  }
-
-  // Private
-
-  private setScene = (evt: any) => {
-    this.setState(() => ({
-      scene: evt.value
-    }));
-  };
-
-  // Getters / Setters
-
-  get componentState(): SidePanelState {
-    return this.state as SidePanelState;
-  }
+  const hasScene = scene !== null;
+  const HierarchyName = 'Hierarchy - ' + (hasScene ? `${scene?.name}` : 'No Scene');
+  return (
+    <div id='SidePanel' key='SidePanel'>
+      <Accordion label={HierarchyName} open={true}>
+      <>
+        {hasScene && <ContainerObject child={scene!} three={props.three} />}
+      </>
+    </Accordion>
+    <Inspector three={props.three} />
+  </div>
+  );
 }
--- types/editor/sidePanel/SidePanel.d.ts
+++ types/editor/sidePanel/SidePanel.d.ts
@@ -1,11 +1,3 @@
-import { Component, ReactNode } from 'react';
 import '../scss/_sidePanel.scss';
 import { SidePanelState } from './types';
-export default class SidePanel extends Component<SidePanelState> {
-    private three;
-    constructor(props: SidePanelState);
-    componentWillUnmount(): void;
-    render(): ReactNode;
-    private setScene;
-    get componentState(): SidePanelState;
-}
+export default function SidePanel(props: SidePanelState): import("react/jsx-runtime").JSX.Element;
