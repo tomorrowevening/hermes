3e6897babf34893e939674fb722a40e40eff1817
Colin Duffy <colin@tomorrowevening.com>
2024-04-12 16:02:54 -0700

    feat(numbers): draggable fields

--- src/editor/sidePanel/inspector/InspectNumber.tsx
+++ src/editor/sidePanel/inspector/InspectNumber.tsx
@@ -1,5 +1,7 @@
 import { RefObject, useEffect, useRef, useState } from 'react';
 import { InspectorFieldType } from './InspectorField';
+import DragNumber from './utils/DragNumber';
+import { noop } from '@/core/types';
 
 export interface InspectNumberProps {
   alt?: string
@@ -20,62 +22,19 @@ export default function InspectNumber(props: InspectNumberProps) {
   const inputRef = useRef<HTMLInputElement>(null);
   const sliderRef = useRef<HTMLInputElement>(null);
 
-  // States
-  const [fieldValue, setFieldValue] = useState(props.value);
-
-  // Handlers
-  const onChange = (evt: any) => {
-    const value = Number(evt.target.value);
-    setFieldValue(value);
-    if (props.onChange !== undefined) props.onChange(props.prop, value);
-  };
-
-  // Mouse dragging
-  useEffect(() => {
-    let mouseDown = false;
-    let mouseStart = -1;
-    let valueStart = 0;
-    let value = Number(fieldValue);
-
-    const onMouseDown = (evt: MouseEvent) => {
-      mouseDown = true;
-      valueStart = Number(value);
-      mouseStart = evt.clientX;
-    };
-
-    const onMouseMove = (evt: MouseEvent) => {
-      if (!mouseDown) return;
-      const deltaAmt = props.step !== undefined ? props.step : 1;
-      const delta = (evt.clientX - mouseStart) * deltaAmt;
-      value = Number((valueStart + delta).toFixed(4));
-      if (props.min !== undefined) value = Math.max(value, props.min);
-      if (props.max !== undefined) value = Math.min(value, props.max);
-      if (inputRef.current !== null) inputRef.current.value = value.toString();
-      if (props.type === 'range') {
-        if (sliderRef.current !== null) sliderRef.current.value = value.toString();
-      }
+  // Hooks
+  const inputValue = DragNumber({
+    label: props.labelRef,
+    input: inputRef,
+    sliderRef: sliderRef,
+    defaultValue: props.value,
+    min: props.min,
+    max: props.max,
+    step: props.step,
+    onChange: (value: number) => {
       if (props.onChange !== undefined) props.onChange(props.prop, value);
-    };
-
-    const onMouseUp = () => {
-      mouseDown = false;
-    };
-
-    const onRightClick = () => {
-      mouseDown = false;
-    };
-
-    props.labelRef.current?.addEventListener('mousedown', onMouseDown, false);
-    document.addEventListener('mouseup', onMouseUp, false);
-    document.addEventListener('mousemove', onMouseMove, false);
-    document.addEventListener('contextmenu', onRightClick, false);
-    return () => {
-      props.labelRef.current?.removeEventListener('mousedown', onMouseDown);
-      document.removeEventListener('mouseup', onMouseUp);
-      document.removeEventListener('mousemove', onMouseMove);
-      document.removeEventListener('contextmenu', onRightClick);
-    };
-  }, [fieldValue]);
+    }
+  });
 
   return (
     <>
@@ -85,27 +44,40 @@ export default function InspectNumber(props: InspectNumberProps) {
           className={props.className}
           ref={inputRef}
           type='number'
-          value={fieldValue}
+          value={inputValue}
           min={props.min}
           max={props.max}
           step={props.step}
           disabled={props.disabled}
-          onChange={onChange}
+          onChange={(evt: any) => {
+            const value = Number(evt.target.value);
+            if (props.onChange !== undefined) props.onChange(props.prop, value);
+          }}
         />
       )}
 
       {props.type === 'range' && (
         <>
-          <input type='text' value={fieldValue.toString()} onChange={onChange} disabled={props.disabled} ref={inputRef} className='min' />
+          <input
+            type='text'
+            value={inputValue.toString()}
+            disabled={props.disabled}
+            ref={inputRef}
+            className='min'
+            onChange={(evt: any) => {
+              const value = Number(evt.target.value);
+              if (props.onChange !== undefined) props.onChange(props.prop, value);
+            }}
+          />
           <input
             disabled={props.disabled}
             type='range'
-            value={fieldValue}
+            value={inputValue}
             min={props.min}
             max={props.max}
             step={props.step}
-            onChange={onChange}
             ref={sliderRef}
+            onChange={noop}
           />
         </>
       )}
