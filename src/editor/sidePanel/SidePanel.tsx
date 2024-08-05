7e7bf184749259526ce1b70a5266ca864f8a13d6
Colin Duffy <colin@tomorrowevening.com>
2024-08-04 18:25:56 -0700

    fix(sidePanel): useStates

--- src/editor/sidePanel/SidePanel.tsx
+++ src/editor/sidePanel/SidePanel.tsx
@@ -11,10 +11,17 @@ import Inspector from './inspector/Inspector';
 
 export default function SidePanel(props: SidePanelState) {
   const [scenes] = useState<RemoteObject[]>([]);
+  const [sceneComponents] = useState<any[]>([]);
   const [lastUpdate, setLastUpdate] = useState(0);
 
   const onAddScene = (evt: any) => {
-    scenes.push(evt.value);
+    const scene = evt.value;
+    scenes.push(scene);
+    sceneComponents.push(
+      <Accordion label={`Scene: ${scene.name}`} open={true} key={Date.now()} canRefresh={true}>
+        <ContainerObject child={scene} scene={scene} three={props.three} />
+      </Accordion>
+    );
     setLastUpdate(Date.now());
   };
 
@@ -23,30 +30,33 @@ export default function SidePanel(props: SidePanelState) {
     for (let i = 0; i < scenes.length; i++) {
       if (scene.uuid === scenes[i].uuid) {
         scenes.splice(i, 1);
+        sceneComponents.splice(i, 1);
         setLastUpdate(Date.now());
         return;
       }
     }
   };
 
+  const onAddGroup = (evt: any) => {
+    console.log('add group', evt);
+  };
+
+  const onRemoveGroup = (evt: any) => {
+    console.log('remove group', evt);
+  };
+
   useEffect(() => {
     debugDispatcher.addEventListener(ToolEvents.ADD_SCENE, onAddScene);
     debugDispatcher.addEventListener(ToolEvents.REMOVE_SCENE, onRemoveScene);
+    debugDispatcher.addEventListener(ToolEvents.ADD_GROUP, onAddGroup);
+    debugDispatcher.addEventListener(ToolEvents.REMOVE_GROUP, onRemoveGroup);
     return () => {
       debugDispatcher.removeEventListener(ToolEvents.ADD_SCENE, onAddScene);
       debugDispatcher.removeEventListener(ToolEvents.REMOVE_SCENE, onRemoveScene);
+      debugDispatcher.removeEventListener(ToolEvents.ADD_GROUP, onAddGroup);
+      debugDispatcher.removeEventListener(ToolEvents.REMOVE_GROUP, onRemoveGroup);
     };
   }, []);
-
-  // Components
-  const sceneComponents: any[] = [];
-  scenes.forEach((scene: any, index: number) => {
-    sceneComponents.push(
-      <Accordion label={`Scene: ${scene.name}`} open={true} key={`scene_${index}`} canRefresh={true}>
-        <ContainerObject child={scene} scene={scene} three={props.three} />
-      </Accordion>
-    );
-  });
 
   return (
     <div id='SidePanel' key={`SidePanel ${lastUpdate}`}>
