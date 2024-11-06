80f4159ffbdfd8a155c5d4750dbbbab1733568ee
Colin Duffy <colin@tomorrowevening.com>
2024-11-06 10:54:04 -0800

    fix(theatre): custom UI

--- src/editor/theatreUtils.ts
+++ src/editor/theatreUtils.ts
@@ -69,17 +69,37 @@ export function theatreEditorApp(app: Application, theatre: RemoteTheatre, studi
 
 // Call this after the Theatre's studio has inited (onload is good)
 export function customizeTheatreElements() {
-  const theatreEl = document.getElementById('theatrejs-studio-root')?.shadowRoot?.getElementById('pointer-root')?.children[0];
-  const headerEl = theatreEl?.children[1] as HTMLDivElement;
-  headerEl.style.justifyContent = 'left';
+  setTimeout(() => {
+    const root = document.getElementById('theatrejs-studio-root');
+    if (root === null) return;
 
-  const rightBtns = headerEl.children[1] as HTMLDivElement;
-  rightBtns.style.transform = 'translateX(10px)';
-  while (rightBtns.children.length > 1) {
-    rightBtns.removeChild(rightBtns.children[0]);
-  }
+    if (root.shadowRoot === null) return;
 
-  const exportEl = theatreEl?.children[3] as HTMLDivElement;
-  exportEl.style.top = '0';
-  exportEl.style.right = '300px';
+    const pointerRoot = root.shadowRoot.getElementById('pointer-root');
+    if (pointerRoot === null) return;
+
+    const theatreEl = pointerRoot.children[0];
+    if (theatreEl === null) return;
+
+    const headerEl = theatreEl.children[1] as HTMLDivElement;
+    headerEl.style.justifyContent = 'left';
+
+    try {
+      const rightBtns = headerEl.children[1] as HTMLDivElement;
+      rightBtns.style.transform = 'translateX(10px)';
+      while (rightBtns.children.length > 1) {
+        rightBtns.removeChild(rightBtns.children[0]);
+      }
+    } catch (_) {
+      //
+    }
+
+    try {
+      const exportEl = theatreEl.children[3] as HTMLDivElement;
+      exportEl.style.top = '0';
+      exportEl.style.right = '300px';
+    } catch (_) {
+      //
+    }
+  }, 1000);
 }
