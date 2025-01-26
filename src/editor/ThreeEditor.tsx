350e8eaadc280571959d87ddea08eea7fe70f45e
Colin Duffy <colin@tomorrowevening.com>
2025-01-25 17:37:32 -0800

    fix(editor): remove example dropdown/draggable

--- src/editor/ThreeEditor.tsx
+++ src/editor/ThreeEditor.tsx
@@ -3,8 +3,6 @@ import Editor from './Editor';
 import MultiView from './multiView/MultiView';
 import RemoteThree from '@/core/remote/RemoteThree';
 import SidePanel from './sidePanel/SidePanel';
-import Dropdown from './components/Dropdown';
-import Draggable from './components/Draggable';
 
 interface ThreeEditorProps {
   three: RemoteThree
@@ -16,48 +14,7 @@ interface ThreeEditorProps {
 
 export default function ThreeEditor(props: ThreeEditorProps) {
   return (
-    <Editor
-      header={(
-        <>
-          <Dropdown
-            title='Options'
-            options={[
-              {
-                type: 'option',
-                title: 'Option 1',
-                value: 'Option 1',
-              },
-              {
-                type: 'option',
-                title: 'Option 2',
-                value: 'Option 2',
-              },
-              {
-                type: 'option',
-                title: 'Option 3',
-                value: 'Option 3',
-              },
-            ]}
-            onSelect={(value: any) => {
-              console.log('Option >', value);
-            }}
-          />
-          <Draggable
-            title='Draggable'
-            options={[
-              'Drag 1',
-              'Drag 2',
-              'Dragggggable 3',
-              'Drag 4',
-              'Drag 5',
-            ]}
-            onDragComplete={(options: string[]) => {
-              console.log('Draggable >', options);
-            }}
-          />
-        </>
-      )}
-    >
+    <Editor>
       <>
         <MultiView
           three={props.three}
