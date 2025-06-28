9f6d3f4a72bbb6ffcda78ca2de0c266421024b1b
Colin Duffy <colin@tomorrowevening.com>
2025-06-28 16:11:36 -0700

    feat(npm): v0.0.140

--- vite.config.ts
+++ vite.config.ts
@@ -19,7 +19,6 @@ export default defineConfig({
         'framer-motion',
         '@theatre/core',
         '@theatre/studio',
-        'postprocessing'
       ],
       output: {
         globals: {
