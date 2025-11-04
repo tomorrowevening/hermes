0fcbbad8b54b65bf8e7d6f51c50b0d24b300dfff
Colin Duffy <colin@tomorrowevening.com>
2025-11-03 19:31:20 -0800

    fix(class): removed

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ export * from './webworkers/EventHandling';
 export * from './webworkers/ProxyManager';
 export * from './core/Application';
 export { default as AppRunner } from './core/AppRunner';
-export { default as BaseScene } from './core/BaseScene';
 export { default as BaseRemote } from './core/remote/BaseRemote';
 export * from './editor/theatreUtils';
 export { default as RemoteTheatre } from './core/remote/RemoteTheatre';
