79e0ba9e64635857895dedf696eeffcf7409a729
Colin Duffy <colin@tomorrowevening.com>
2025-12-23 13:50:56 -0800

    feat(uv): transparent

--- src/editor/multiView/UVMaterial.ts
+++ src/editor/multiView/UVMaterial.ts
@@ -10,36 +10,38 @@ const vertex = `#include <common>
 #include <clipping_planes_pars_vertex>
 
 void main() {
-	#include <uv_vertex>
-	#include <color_vertex>
-	#include <morphcolor_vertex>
-	#include <batching_vertex>
+  #include <uv_vertex>
+  #include <color_vertex>
+  #include <morphcolor_vertex>
+  #include <batching_vertex>
 
-	#if defined ( USE_SKINNING )
-		#include <beginnormal_vertex>
-		#include <morphnormal_vertex>
-		#include <skinbase_vertex>
-		#include <skinnormal_vertex>
-		#include <defaultnormal_vertex>
-	#endif
+  #if defined ( USE_SKINNING )
+    #include <beginnormal_vertex>
+    #include <morphnormal_vertex>
+    #include <skinbase_vertex>
+    #include <skinnormal_vertex>
+    #include <defaultnormal_vertex>
+  #endif
 
-	#include <begin_vertex>
-	#include <morphtarget_vertex>
-	#include <skinning_vertex>
-	#include <project_vertex>
-	#include <logdepthbuf_vertex>
-	#include <clipping_planes_vertex>
-	#include <worldpos_vertex>
+  #include <begin_vertex>
+  #include <morphtarget_vertex>
+  #include <skinning_vertex>
+  #include <project_vertex>
+  #include <logdepthbuf_vertex>
+  #include <clipping_planes_vertex>
+  #include <worldpos_vertex>
 }`;
 
 const fragment = `
+uniform float opacity;
 #include <common>
 #include <uv_pars_fragment>
 #include <clipping_planes_pars_fragment>
 
 void main() {
-	#include <clipping_planes_fragment>
-	gl_FragColor = vec4(vec3(vUv, 0.0), 1.0);
+  #include <clipping_planes_fragment>
+  if (opacity < 0.015) discard;
+  gl_FragColor = vec4(vec3(vUv, 0.0), opacity);
 }`;
 
 export default class UVMaterial extends ShaderMaterial {
@@ -48,8 +50,12 @@ export default class UVMaterial extends ShaderMaterial {
       defines: {
         USE_UV: ''
       },
+      uniforms: {
+        opacity: { value: 1 },
+      },
       vertexShader: vertex,
       fragmentShader: fragment,
+      transparent: true,
     });
   }
 }
