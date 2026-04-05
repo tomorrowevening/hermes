317053a8bb4f6185155cccc384acf527e397ebe6
Colin Duffy <colin@tomorrowevening.com>
2026-04-05 12:45:34 -0700

    fix(example): steamlined

--- src/example/components/AppWrapper.tsx
+++ src/example/components/AppWrapper.tsx
@@ -1,37 +1,31 @@
+// Libs
 import { useEffect, useState } from 'react';
+// Models
 import { IS_DEV, IS_EDITOR } from '../constants';
-import Application from '../../core/Application';
-import RemoteTheatre from '../../core/remote/RemoteTheatre';
-import RemoteThree from '../../core/remote/RemoteThree';
+// 
+import ExampleApplication from '../three/ExampleApplication';
 import RemoteSetup from './RemoteSetup';
 import CustomEditor from '../CustomEditor';
-import Wrapper from './Wrapper';
+import Loader from './Loader';
+import App from './App';
+import { loadAssets } from '../three/loader';
 
 export default function AppWrapper() {
-  const [app, setApp] = useState<Application | null>(null);
+  const [app, setApp] = useState<ExampleApplication | null>(null);
 
   useEffect(() => {
-    const instance = new Application();
-    instance.detectSettings(IS_DEV, IS_EDITOR).then(() => {
-      // TheatreJS
-      instance.addComponent('theatre', new RemoteTheatre(IS_DEV, IS_EDITOR));
-
-      // ThreeJS
-      const three = new RemoteThree('Hermes Example', IS_DEV, IS_EDITOR);
-      instance.addComponent('three', three);
-
-      // Ready
-      setApp(instance);
-    });
+    const instance = new ExampleApplication('Hermes Example', IS_DEV, IS_EDITOR);
+    instance.detectSettings().then(() => setApp(instance));
   }, []);
 
   return (
     <>
       {app !== null && (
         <>
-          {app.debugEnabled && <RemoteSetup app={app} />}
-          {app.editor && <CustomEditor app={app} />}
-          <Wrapper app={app} />
+          {IS_DEV && <RemoteSetup app={app} />}
+          {IS_EDITOR && <CustomEditor app={app} />}
+          <Loader app={app} onLoad={loadAssets} />
+          {app.isApp && <App app={app} />}
         </>
       )}
     </>
