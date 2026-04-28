34b999f1a1bd40eddb12f5272739682b53e65477
Colin Duffy <colin@tomorrowevening.com>
2026-04-27 18:29:20 -0700

    feat(npm): v0.1.37

--- types/editor/HermesApp.d.ts
+++ types/editor/HermesApp.d.ts
@@ -1,12 +1,9 @@
 import { ReactNode } from 'react';
-import { Scene } from 'three';
 import Application from '../core/Application';
+import RemoteThree from '../core/remote/RemoteThree';
 type HermesAppProps = {
     app: Application;
-    scenes: Map<string, any>;
-    onSceneAdd?: (scene: any) => void;
-    onSceneUpdate?: (scene: any) => void;
-    onSceneResize?: (scene: Scene, width: number, height: number) => void;
+    renderEditor?: (three: RemoteThree) => ReactNode;
     onLoad?: (app: Application) => Promise<void>;
     renderLoading?: ReactNode;
     children?: (app: Application) => ReactNode;
